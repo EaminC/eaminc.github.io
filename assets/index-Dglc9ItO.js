ad1e09fc2c293296d293e39ee3dfe941b99f5b85
Yiming Cheng <84763321+EaminC@users.noreply.github.com>
2025-05-10 03:47:25 -0500

    Update index-Dglc9ItO.js

--- assets/index-Dglc9ItO.js
+++ assets/index-Dglc9ItO.js
@@ -9512,7 +9512,7 @@ const S0 = () => {
             icon: "icon-wodeboke",
             animate: "CV",
             color: "#fe8599",
-            href: "./intro.html",
+            href: "../assets/resume_20250508.pdf",
           },
           {
             icon: "icon-github",
